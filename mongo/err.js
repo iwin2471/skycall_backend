ba221ebc5b760f056ee4688c2f2eb0721925e880
iwin2471 <iwin12372@gmail.com>
2018-10-02 14:07:05 +0900

    add: unexpect param check

--- mongo/err.js
+++ mongo/err.js
@@ -1,18 +1,29 @@
 module.exports = (Users, Boards, rndString) => {
   let user_params = ['id', 'passwd', 'name', 'phone_number', "business_reg_number"];
   const balju_params = ['business_name']
-  const oder_params = ['accept_height', 'business_start_day', 'business_reg_number', 'business_area']
+  const oder_params = ['accept_height', 'business_start_day', 'business_area']
   const boards_params = ['title', 'contents'];
 
   Users.pre('save', async function (next) {
     let user = this;
     let check_param;
-    if (user.type)
-      user_params += balju_params;
-    else
-      user_params += oder_params;
+    let result; //파라미터가 있는지 확인하여 넣을 
 
-    let result = await user_params.every(str => user[str] != undefined && user[str] != null && user[str].length > 0);
+    if (user.type) {
+      check_param = await oder_params.every(str => user.hasOwnProperty(str));
+
+      if (check_param)
+        next(new paramsError("wrong param"));
+
+      user_params += balju_params;
+    } else {
+      if (user.hasOwnProperty(balju_params[0]))
+        next(new paramsError("wrong param"));
+      user_params += oder_params;
+    }
+
+    result = await user_params.every(str => user.hasOwnProperty(str) && user[str] != null && user[str].length > 0);
+
     if (!result)
       next(new paramsError("param missing or null"));
     else {
